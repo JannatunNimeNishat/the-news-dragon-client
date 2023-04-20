663ee1b4b9530a457436888389b9854fa2ce33bf
JannatunNimeNishat <nishatnime100@gmail.com>
2023-04-20 22:16:26 +0600

    NewsLayout created

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
-import Footer from '../pages/Shared/Footer/Footer';
-import { Outlet } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
-import LeftNav from '../pages/Shared/LeftNav/LeftNav';
-import RightNav from '../pages/Shared/RightNav/RightNav';
+import Footer from '../pages/Shared/Footer/Footer';
 
-const Main = () => {
+const NewsLayout = () => {
     return (
         <div>
             <Header></Header>
             <Container>
                 <Row>
-                    <Col md={3}>
-                        <LeftNav></LeftNav>
-                    </Col>
-                    <Col md={6}>
+                    <Col md={9}>
                         <Outlet></Outlet>
                     </Col>
                     <Col md={3}>
@@ -29,4 +23,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default NewsLayout;
